345dfbc8232f75aee8516eaee8384c771e2340bf
Asael Christian <dev.asaelchristian@gmail.com>
2024-07-03 20:01:22 -0300

    Em processo...

---
+++ cadastro.js
@@ -0,0 +1,34 @@
+/*Código Javascript*/
+
+
+function onYes(){  
+    Swal.fire({
+title: "Você deseja receber notificações da Paris Chansons?",
+text: "Somos a maior e melhor companhia do ramo imobiliário do mundo, segundo a última pesquisa da Forbes feita em Julho de 2024.",
+color: "#FFFFFF",
+icon: "question",
+background: "url('imagens/city-night.jpg') no-repeat center center",
+showCancelButton: true,
+cancelButtonText: "Não!",
+confirmButtonColor: "#3085d6",
+cancelButtonColor: "#d33",
+confirmButtonText: "Sim!"
+}).then((result) => {
+if (result.isConfirmed) {
+Swal.fire({
+title: "Confirmado!",
+text: "Enviaremos nossas últimas novidades a você.",
+icon: "success"
+});
+}
+});
+}
+
+function buttonM(){
+Swal.fire({
+    icon: "error",
+    title: "Oops...",
+    text: "Não foi possível encontrar sua busca :(",
+    footer: '<b>Trágico!</b>'
+  });
+}
